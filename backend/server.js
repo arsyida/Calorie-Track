62d98183abe5135203432825402a1c76e13fdcb8
arsyida <122206139+arsyida@users.noreply.github.com>
2024-06-11 12:28:10 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,34 +5,31 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const PORT = process.env.PORT;
+
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 
 const uri = process.env.URL_DB;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-async function runDB() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (error){
-    // Ensures that the client will close when you finish/error
-    console.error('Error:', error);
-  }
+async function connectDB() {
+      try {
+          await client.connect();
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
-runDB();
+connectDB();
 
 // router
 app.get("/Activity", async(req, res)=>{
@@ -41,7 +38,7 @@ app.get("/Activity", async(req, res)=>{
 })
 
 app.get("/Food", async(req, res)=>{
-    const data = await client.db('caalories').collection('Food And Drink').find().sort({name:1}).toArray()
+    const data = await client.db('calories').collection('Food And Drink').find().sort({name:1}).toArray()
     res.json(data)
 })
 
